f2fa5140c726a8ccb98ba756ab9379c78af745f0
Rafael <rafael@iMac.local>
2021-03-04 13:14:03 -0300

    Coding

--- index.ts
+++ index.ts
@@ -1,2 +1,41 @@
 import * as crypto from 'crypto';
 
+class Transaction {
+    constructor(
+        public amount: number,
+        public payer: string,
+        public payee: string
+    ) {}
+
+    toString() {
+        return JSON.stringify(this);
+    }
+}
+
+class Block {
+    constructor(
+        public prevHash: string,
+        public transaction: Transaction,
+        public ts = Date.now()
+    ) {}
+
+    get hash() {
+        const str = JSON.stringify(this);
+        const hash = crypto.createHash('SHA256');
+        hash.update(str).end();
+        return hash.digest('hex');
+    }
+}
+
+class Chain {
+    public static instance = new Chain();
+
+    chain: Block[];
+
+    constructor() {
+        this.chain = [new Block(null, new Transaction(100, 'genesis', 'rafacost'))];
+    }
+}
+
+class Wallet {
+}
